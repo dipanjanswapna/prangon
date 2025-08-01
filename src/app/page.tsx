8a3a3354dfb32eb9f8755a05febaed923259714f
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-01 07:06:48 +0000

    for er colour rgb(235, 89, 56) koro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <div className="relative z-10 text-center px-4" style={{ color: 'rgb(184, 172, 152)' }}>
         <p className="text-sm md:text-base tracking-[0.3em] mb-4">WELCOME TO PRANGON CENTRE</p>
         <h1 className="font-headline text-6xl md:text-8xl lg:text-9xl font-black uppercase leading-none tracking-tighter">
-          smile for miles
+          smile <span style={{ color: 'rgb(235, 89, 56)' }}>for</span> miles
         </h1>
       </div>
     </div>
