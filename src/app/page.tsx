2da8c377e1e0b65022b7909cd16adcb1542a9fb7
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-01 07:01:17 +0000

    Making Good Shit Since 2009 replace to smile for miles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,7 @@ export default function Home() {
       <div className="relative z-10 text-center px-4" style={{ color: 'rgb(184, 172, 152)' }}>
         <p className="text-sm md:text-base tracking-[0.3em] mb-4">PRANGON CENTRE</p>
         <h1 className="font-headline text-6xl md:text-8xl lg:text-9xl font-black uppercase leading-none tracking-tighter">
-          Making<br />
-          <span className="text-primary">Good Shit</span><br />
-          Since<br />
-          2009
+          smile for miles
         </h1>
       </div>
     </div>
