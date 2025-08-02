17944f2b531c58a9d207ee7da1d8f85a8487de8b
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-08-02 06:58:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,15 +1,17 @@
-import "dotenv/config";
+import 'dotenv/config';
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
     const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT;
     if (!serviceAccountJson) {
-        throw new Error("The FIREBASE_SERVICE_ACCOUNT environment variable is not set.");
+      throw new Error(
+        'The FIREBASE_SERVICE_ACCOUNT environment variable is not set.'
+      );
     }
     const serviceAccount = JSON.parse(serviceAccountJson);
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
+      credential: admin.credential.cert(serviceAccount),
     });
   } catch (error) {
     console.error('Firebase Admin Initialization Error', error);
