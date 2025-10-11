e179af74d03f9168e7b5b2d65c0a6d4d3f9f756b
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-10-11 09:01:45 +0000

    hoy ni

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -6,6 +6,16 @@ if (!admin.apps.length) {
   try {
     const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
+    if (!process.env.FIREBASE_PROJECT_ID) {
+      throw new Error('FIREBASE_PROJECT_ID is not set.');
+    }
+    if (!process.env.FIREBASE_CLIENT_EMAIL) {
+      throw new Error('FIREBASE_CLIENT_EMAIL is not set.');
+    }
+    if (!privateKey) {
+        throw new Error('FIREBASE_PRIVATE_KEY is not set.');
+    }
+
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
