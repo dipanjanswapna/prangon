7cc2e6d3d6fd7cec9ec71fb2cf96e8d90c8f1283
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-10-11 02:24:29 +0000

    same problem show admin panel ke all device responsive koro

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -69,7 +69,7 @@ export default function AdminLayout({
                 </SidebarMenu>
             </SidebarContent>
         </Sidebar>
-        <div className="flex flex-col flex-1 sm:gap-4 sm:py-4 sm:pl-14">
+        <div className="flex flex-col flex-1 sm:gap-4 sm:py-4">
             <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
                 <SidebarTrigger className="sm:hidden" />
                 <div className="relative ml-auto flex-1 md:grow-0">
